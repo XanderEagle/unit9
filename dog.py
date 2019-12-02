3dcbd979def8c23d4053c32d8114e66a83daa4e3
Xander <alexander.eagle@ssfs.org>
2019-12-02 09:00:37 -0500

    worked on dog exercise

---
+++ dog.py
@@ -0,0 +1,10 @@
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def print_name(self):
+        print(self.name)
---
+++ dog_test.py
@@ -0,0 +1,5 @@
+import dog
+
+my_dog = dog.Dog("Buck")
+
+my_dog.print_name()
