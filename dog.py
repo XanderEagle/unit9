b99debe586dd643e381d8a1a11e0e3071a2d7ce9
Xander <alexander.eagle@ssfs.org>
2019-12-03 10:14:32 -0500

    did dog exercise

--- dog.py
+++ dog.py
@@ -2,9 +2,22 @@ class Dog:
 
     def __init__(self, name):
         self.name = name
+        self.trick_list = []
 
-    def get_name(self):
+    def dog_name(self):
         return self.name
 
     def print_name(self):
         print(self.name)
+
+    def sit(self):
+        print(self.name + " sits")
+        self.trick_list.append("sits")
+
+    def roll_over(self):
+        print(self.name + " rolls over")
+        self.trick_list.append("roll over")
+
+    def shake(self):
+        print(self.name + " shakes")
+        self.trick_list.append("shake")
--- dog_test.py
+++ dog_test.py
@@ -3,3 +3,11 @@ import dog
 my_dog = dog.Dog("Buck")
 
 my_dog.print_name()
+
+my_dog.sit()
+
+my_dog.roll_over()
+
+my_dog.shake()
+
+print(my_dog.trick_list)
